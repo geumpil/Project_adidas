90f0b59004d3c189d1a6590caebbdd709608a0f8
geumpil <86139036+geumpil@users.noreply.github.com>
2023-11-30 03:29:22 +0900

    231130_webproject

---
+++ adidas/js/header_expand.js
@@ -0,0 +1,18 @@
+const headerExpand = document.querySelector('header > div');
+const listHoverActive = document.getElementsByClassName('main-menu-list');
+
+for(let i = 0; i < listHoverActive.length; i++) {
+    listHoverActive[i].addEventListener('mouseover', () => {
+        removeActive();
+        listHoverActive[i].classList.add('hover-active');
+    })
+    listHoverActive[i].addEventListener('mouseleave', () => {
+        removeActive();
+    })
+}
+
+function removeActive() {
+    for(let i = 0; i < listHoverActive.length; i++) {
+        listHoverActive[i].classList.remove('hover-active');
+    }
+}
