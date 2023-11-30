1f749b0e2be06eb46764460b340b19b3ebac6573
geumpil <86139036+geumpil@users.noreply.github.com>
2023-11-30 15:12:14 +0900

    231130_webproject

--- adidas/js/hamberger_nav.js
+++ adidas/js/hamberger_nav.js
@@ -1,21 +1,32 @@
 const hambergerNav = document.querySelector('.hamberger-nav');
 const hambergerAct = document.querySelector('.hamberger-click');
-const closeButton = document.querySelector('.mob-menu-close');
+const closeButton = document.getElementsByClassName('mob-menu-close');
 const listButton = document.getElementsByClassName('list-btn');
-const listSubmenu = document.getElementsByClassName('.list-submenu-area');
+const listSubmenu = document.getElementsByClassName('list-submenu-area');
 const prevSubButton = document.querySelectorAll('.pre-top-menu');
 
 hambergerNav.addEventListener('click', () => {
     openHambergerMenu();
 })
 
-closeButton.addEventListener('click', () => {
-    closeHambergerMenu();
-})
+for(let i = 0 ; i < closeButton.length ; i ++) {
+    closeButton[i].addEventListener('click', () => {
+        closeHambergerMenu();
+    })
+}
 
 for(let i = 0; i < listButton.length; i++) {
     listButton[i].addEventListener('click', () => {
-        openSubmenu();
+        console.log(
+
+            listButton[i].parentElement.querySelector('.list-submenu-area').classList.add('sublist-act')
+        )
+    })
+}
+
+for(let i = 0 ; i < prevSubButton.length ; i ++) {
+    prevSubButton[i].addEventListener('click',() => {
+        prevSubmenu();
     })
 }
 
@@ -28,11 +39,13 @@ function closeHambergerMenu() {
     hambergerAct.classList.remove('nav-active');
 }
 
-function openSubmenu() {
-    for(let i = 0; i < listSubmenu.length; i++)
-    listSubmenu[i].classList.add('sublist-act');
-}
+// function openSubmenu(index) {
+//     listSubmenu[index].classList.add('sublist-act');
+// }
 
 function prevSubmenu() {
-    listSubmenu.classList.remove('sublist-act');
+    for(let i = 0 ; i < listSubmenu.length ; i ++) {
+
+        listSubmenu[i].classList.remove('sublist-act');
+    }
 }
--- adidas/js/slide_act.js
+++ adidas/js/slide_act.js
@@ -1,11 +1,104 @@
-const slideWrap = document.querySelectorAll('.items-wrap');
-const slideImg = document.querySelectorAll('.items-list');
-const prevButton = document.querySelectorAll('.items-prev');
-const nextButton = document.querySelectorAll('.items-next');
-const slideBar = document.querySelectorAll('.items-bar-area');
-const slideView = document.querySelectorAll('.items-slide-view');
+const slideWrap = document.querySelector('.items-wrap');
+const slideImg = document.querySelector('.items-list');
+const prevButton = document.querySelector('.items-prev');
+const nextButton = document.querySelector('.items-next');
+const slideBar = document.querySelector('.items-bar-area');
+const slideView = document.querySelector('.items-slide-view');
+const maxCount = document.querySelector('.total-item');
+const currentCount = document.querySelector('.current-item');
 
 const realSlideCount = slideImg.length;
-const viewCount = 4;
+let viewCount = 4;
 let slideImgSize = '310px';
 let gapSize = '20px';
+let index = 0;
+let loopInterval;
+
+
+for(let i = 0; i < realSlideCount; i++) {
+    const barBtn = document.createElement('div');
+    barBtn.classList.add('items-bar');
+
+    barBtn.addEventListener('click', ()=> {
+        index = i + viewCount;
+        slideApply(true);
+    })
+    slideBar.appendChild(barBtn);
+}
+
+slideApply(false);
+
+function slideApply(animation) {
+    slideWrap.style.transform = `translateX(calc((-${slideImgSize} - ${gapSize}) * ${index}))`
+    buttonUpdate();
+}
+
+function buttonUpdate() {
+    for(let i = 0; i < slideBar.childElementCount; i++) {
+        slideBar.children[i].classList.remove('activate');
+        slideBar.children[index].classList.add('activate');
+    }
+}
+
+
+let moveAble = true;
+
+prevButton.addEventListener('click', prev);
+nextButton.addEventListener('click', next);
+
+function prev() {
+    if(moveAble) {
+        moveAble = false;
+        index--;
+        if(index < 0) {
+            index = 0;
+        }
+        setCurrentSlideCount();
+        slideApply(true);
+
+        setTimeout(() => {
+            moveAble = true;
+        }, 1000);
+    }
+}
+
+function next() {
+    if(moveAble){
+        moveAble = false; 
+        index++;
+        if(index > slideWrap.childElementCount - viewCount){
+            index = slideWrap.childElementCount - viewCount
+        }
+        setCurrentSlideCount();
+        slideApply(true);
+
+        setTimeout(() => {
+            moveAble = true;
+        }, 1000);
+    }
+}
+
+
+setSlideMaxCount();
+
+function setCurrentSlideCount() {
+    console.log(currentCount.innerText = `${String(index + 1).padStart(2,'0')}`);
+}
+function setSlideMaxCount() {
+    maxCount.innerText = `${String(slideWrap.childElementCount-viewCount+1).padStart(2,'0')}`
+}
+
+window.addEventListener('resize', ()=>{
+
+    if(window.innerWidth < 744) {
+        slideImgSize = '100vw';
+        viewCount = 2;
+    }else if(window.innerWidth < 1364) {
+        slideImgSize = '100vw';
+        viewCount = 3
+    }else {
+        viewCount = 4;
+    }
+    setCurrentSlideCount();
+    setSlideMaxCount();
+})
