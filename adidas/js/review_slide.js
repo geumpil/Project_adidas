04f6a161759d50481e9a41d63779e2f456502571
geumpil <jgp0801@naver.com>
2023-12-13 20:58:39 +0900

    231213_webproject

--- adidas/js/review_slide.js
+++ adidas/js/review_slide.js
@@ -8,3 +8,51 @@ let reViewCount = 4;
 let reSlideImgSize = 0; 
 let reGapSize = 0;
 let index = 0;
+let moveActivate = true;
+
+reSlideApply(false);
+
+// 리사이징
+
+// 슬라이드 이동
+
+function reSlideApply(animation) {
+    reSlideWrap.computedStyleMap.transfrm = `translateX(calc((${reSlideImgSize} - ${reGapSize}) * ${index}))`;
+}
+
+// 버튼(클릭) 이동
+
+rePrev.addEventListener('click', rePrevMove);
+reNext.addEventListener('click', reNextMove);
+
+function rePrevMove() {
+    if(moveActivate) {
+        moveActivate = false;
+        index--;
+        if(index < 0) {
+            index = 0;
+        }
+        reSlideApply(true);
+
+        setTimeout(() => {
+            moveActivate = true;
+        }, 500);
+    }
+}
+
+function reNextMove() {
+    if(moveActivate) {
+        moveActivate = false;
+        index++;
+        if(index > reSlideWrap.childElementCount - reViewCount) {
+            index = reSlideWrap.childElementCount - reViewCount;
+        }
+        reSlideApply(true);
+
+        setTimeout(() => {
+            moveActivate = true;
+        }, 500);
+    }
+}
+
+// 클론 생성
