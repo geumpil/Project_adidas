788542bbb3735fae009841e850a89780180ff843
geumpil <86139036+geumpil@users.noreply.github.com>
2023-12-13 20:05:44 +0900

    231213_webproject

---
+++ adidas/js/review_slide.js
@@ -0,0 +1,10 @@
+const reSlideWrap = document.querySelector('.review-slide');
+const reSlideImg = document.querySelector('.review-list');
+const rePrev = document.querySelector('.review-prev');
+const reNext = document.querySelector('.review-next');
+const reSlideView = document.querySelector('.review-wrap');
+
+let reViewCount = 4;
+let reSlideImgSize = 0; 
+let reGapSize = 0;
+let index = 0;
